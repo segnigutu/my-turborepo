743085eb5dc8ece8705143d819a4888e3e08b8bd
segnigutu <136971056+segnigutu@users.noreply.github.com>
2026-04-10 22:14:55 +0300

    Implemented task functionality with state management

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,44 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
 import { Navbar } from "@repo/ui/navbar";
-import { Input } from "@repo/ui/input";
 
 export default function Home() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState<string[]>([]);
+
+ const addTask = () => {
+  if (task.trim() === "") return;
+console.log("clicked");
+  setTasks((prev) => [...prev, task]);  
+  setTask("");
+};
+
   return (
     <div>
       <Navbar />
 
       <Card>
         <h3>Add Task</h3>
-        <Input placeholder="Enter task..." />
-        <Button text="Click Me" />
+
+        <input
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter task..."
+          style={{ padding: "10px", margin: "5px" }}
+        />
+
+      <Button onClick={addTask}>Add Task</Button>
+      </Card>
+
+      <Card>
+        <h3>Task List</h3>
+
+        {tasks.map((t, index) => (
+          <div key={index}>{t}</div>
+        ))}
       </Card>
     </div>
   );
